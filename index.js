132dc8a9558d152daac4081446ebcadcfa5368c1
Rishi <rishiekandala@gmail.com>
2024-01-29 22:58:40 +0530

    established connection from frontend to backend

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-const http = require("http");
+import express from "express";
 
-console.log(http);
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+//get a list of 5 jokes
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A joke",
+      content: "This is a joke",
+    },
+    {
+      id: 2,
+      title: "A joke",
+      content: "This is another joke",
+    },
+    {
+      id: 3,
+      title: "A joke",
+      content: "This is a third joke",
+    },
+    {
+      id: 4,
+      title: "A joke",
+      content: "This is a fourth joke",
+    },
+    {
+      id: 5,
+      title: "A joke",
+      content: "This is a fifth joke",
+    },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 3003;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
